51404307aea80020dc234d7b05ec619c920769ac
Frank Anin <frankanin99@gmail.com>
2024-07-25 22:26:46 +0000

    You used to attend a place like this at some point

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,16 +1,17 @@
 /**
  * Class representing a classroom.
- */
+*/
 export default class ClassRoom {
   /**
    * Create a classroom.
-   * @param {number} maxStudentsSize - The maximum number of students in the classroom.
-   */
+   * @param {number} maxStudentsSize - The maximum number of
+   * students in the classroom.
+  */
   constructor(maxStudentsSize) {
     /**
      * @type {number}
      * @private
-     */
+    */
     this._maxStudentsSize = maxStudentsSize;
   }
 }
