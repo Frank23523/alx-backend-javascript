c62dd0171c23e41ece7384e706cfffa8a77e74ef
Frank Anin <frankanin99@gmail.com>
2024-07-25 21:07:11 +0000

    A building

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,11 +1,7 @@
 export default class Building {
   constructor(sqft) {
-    this.sqft = sqft;
-    if (this.constructor !== Building) {
-      if (typeof this.evacuationWarningMessage !== 'function') {
-        throw new Error('Class extending Building must override evacuationWarningMessage');
-      }
-    }
+    this._sqft = sqft;
+    this.evacuationWarningMessage();
   }
 
   get sqft() {
@@ -13,9 +9,12 @@ export default class Building {
   }
 
   set sqft(value) {
-    if (typeof value !== 'number') {
-      throw new TypeError('sqft must be a number');
-    }
     this._sqft = value;
+  }
+
+  evacuationWarningMessage() {
+    if (this.constructor !== Building) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
   }
 }
