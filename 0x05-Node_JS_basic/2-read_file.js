ffa2da76cf09e635df4faa61b1c606f6e010d1f9
Frank Anin <frankanin99@gmail.com>
2024-09-20 23:46:11 +0000

    Reading a file synchronously with Node JS

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,11 +1,11 @@
 // read the database file synchronously
-const fs = require("fs");
+const fs = require('fs');
 
 function countStudents(path) {
   try {
-    const data = fs.readFileSync(path, "utf8");
+    const data = fs.readFileSync(path, 'utf8');
 
-    const lines = data.split("\n").filter((line) => line.trim() !== "");
+    const lines = data.split('\n').filter((line) => line.trim() !== '');
 
     const students = lines.slice(1);
 
@@ -14,7 +14,7 @@ function countStudents(path) {
     const studentsByField = {};
 
     students.forEach((student) => {
-      const [firstName, , , field] = student.split(",");
+      const [firstName, , , field] = student.split(',');
       if (!studentsByField[field]) {
         studentsByField[field] = [];
       }
@@ -25,11 +25,11 @@ function countStudents(path) {
       console.log(
         `Number of students in ${field}: ${
           students.length
-        }. List: ${students.join(", ")}`
+        }. List: ${students.join(', ')}`,
       );
     }
   } catch (error) {
-    throw new Error("Cannot load the database");
+    throw new Error('Cannot load the database');
   }
 }
 
