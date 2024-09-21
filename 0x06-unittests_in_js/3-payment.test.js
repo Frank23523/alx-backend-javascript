4a185c971e78e0d47dc0f93dfc8147d3048249df
Frank Anin <frankanin99@gmail.com>
2024-09-21 01:31:24 +0000

    Spies

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,16 +1,31 @@
-const { expect } = require('chai');
 const sinon = require('sinon');
+const { expect } = require('chai');
 const Utils = require('./utils');
 const sendPaymentRequestToApi = require('./3-payment');
 
 describe('sendPaymentRequestToApi', () => {
-  it('should use Utils.calculateNumber with correct arguments', () => {
-    const calculateNumberSpy = sinon.spy(Utils, 'calculateNumber');
+  let spy;
 
+  beforeEach(() => {
+    spy = sinon.spy(Utils, 'calculateNumber');
+  });
+
+  afterEach(() => {
+    spy.restore(); // Restore the original function after each test
+  });
+
+  it('should call Utils.calculateNumber with SUM, 100, 20', () => {
     sendPaymentRequestToApi(100, 20);
 
-    expect(calculateNumberSpy.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
+    expect(spy.calledOnce).to.be.true;
+    expect(spy.calledWith('SUM', 100, 20)).to.be.true;
+  });
 
-    calculateNumberSpy.restore();
+  it('should log the total correctly', () => {
+    const consoleSpy = sinon.spy(console, 'log');
+    sendPaymentRequestToApi(100, 20);
+
+    expect(consoleSpy.calledWith('The total is: 120')).to.be.true;
+    consoleSpy.restore();
   });
 });
